a2fe1512694cee3608cfd3ffa889d50eb697df00
haraprasad <haraprasadbehera1808@gmail.com>
2024-01-10 16:10:31 +0530

    127 : create save post feature class added

---
+++ src/main/java/com/myblog/myblog11/service/impl/PostServiceImpl.java
@@ -0,0 +1,12 @@
+package com.myblog.myblog11.service.impl;
+
+import com.myblog.myblog11.entity.Post;
+import com.myblog.myblog11.payload.PostDto;
+import com.myblog.myblog11.service.PostService;
+
+public class PostServiceImpl implements PostService {
+    @Override
+    public PostDto createPostdto(PostDto postDto) {
+        return null;
+    }
+}
